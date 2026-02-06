8e6ed3629881e85afff63d46c7c863f004e87417
Amin <houssni.amin@outlook.fr>
2026-02-06 20:22:21 +0100

    feat: add recipe cards

--- app.js
+++ app.js
@@ -35,4 +35,84 @@ header.appendChild(logo)
 header.appendChild(h1)
 header.appendChild(searchBar)
 
+//MAIN
+const main = document.createElement("main")
+main.className = "w-full bg-[#E5E5E5] grid grid-cols-3 gap-10 py-10 px-[10%]"
+
+async function displayRecipes() {
+  const { recipes } = await import("./recipes.js")
+
+  recipes.forEach((recipe) => {
+    const container = document.createElement("div")
+    container.className =
+      "max-w-[380px] h-auto flex flex-col bg-white rounded-xl overflow-hidden shadow-md relative"
+
+    const time = document.createElement("p")
+    time.innerText = `${recipe.time}min`
+    time.className =
+      "absolute top-5 right-5 bg-[#FFD15B] py-1 px-5 rounded-full"
+
+    const image = document.createElement("img")
+    image.src = `./pictures/${recipe.image}`
+    image.className = "object-cover w-full h-[250px]"
+
+    const textContainer = document.createElement("div")
+    textContainer.className = "m-5"
+
+    const name = document.createElement("h2")
+    name.innerText = recipe.name
+    name.className = " font-bold mb-6 text-lg font-anton "
+
+    const recette = document.createElement("p")
+    recette.innerText = "RECETTE"
+    recette.className = "mb-2 font-bold text-neutral-500 text-xs"
+
+    const description = document.createElement("p")
+    description.innerText = recipe.description
+    description.className = "line-clamp-4 font-normal text-sm"
+
+    const ingredients = document.createElement("p")
+    ingredients.innerText = "INGRÉDIENTS"
+    ingredients.className = "mt-8 mb-2 font-bold text-neutral-500 text-xs "
+
+    const ingredientsGrid = document.createElement("div")
+    ingredientsGrid.className = "grid grid-cols-2 gap-y-4"
+
+    recipe.ingredients.forEach((item) => {
+      const ingredientsBlock = document.createElement("div")
+      ingredientsBlock.className = "flex flex-col"
+
+      const ingredientName = document.createElement("p")
+      ingredientName.innerText = item.ingredient
+      ingredientName.className = "text-sm font-medium font-normal"
+
+      const ingredientQty = document.createElement("p")
+
+      const quantity = item.quantity ? item.quantity : ""
+      const unit = item.unit ? item.unit : ""
+
+      ingredientQty.innerText = `${quantity} ${unit}`
+      ingredientQty.className = "text-sm text-neutral-500"
+
+      ingredientsBlock.appendChild(ingredientName)
+      ingredientsBlock.appendChild(ingredientQty)
+      ingredientsGrid.appendChild(ingredientsBlock)
+    })
+
+    textContainer.appendChild(name)
+    textContainer.appendChild(recette)
+    textContainer.appendChild(description)
+    textContainer.appendChild(ingredients)
+    textContainer.appendChild(ingredientsGrid)
+
+    container.appendChild(time)
+    container.appendChild(image)
+    container.appendChild(textContainer)
+
+    main.appendChild(container)
+  })
+}
+displayRecipes()
+
 app.appendChild(header)
+app.appendChild(main)
